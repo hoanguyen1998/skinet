7af337284ffaa0afa8a331fbd970cbd45e7f37a0
Mark <hoanguyen09101998@gmail.com>
2020-05-07 10:39:38 +0700

    Designing The Shop Page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +7,18 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {
+  constructor() {
     
   }
   
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    // this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+    //   (response: IPagination) => {
+    //   this.products = response.data;
+    // }, error => {
+    //   console.log(error);
+    // });
   }
 }
