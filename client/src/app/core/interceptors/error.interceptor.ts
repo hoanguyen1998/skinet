6841f9af05e4e9c1ade96b921f94dd22a1d72346
Mark <hoanguyen09101998@gmail.com>
2020-05-15 18:18:00 +0700

    Client - Pazzazz

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Router, NavigationExtras } from "@angular/router";
 import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { catchError, delay } from "rxjs/operators";
 import { Injectable } from "@angular/core";
 import { ToastrService } from 'ngx-toastr';
 
@@ -11,6 +11,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
+            delay(1000),
             catchError(error => {
                 if (error) {
                     if (error.status === 400) {
