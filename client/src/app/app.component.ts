5a9e147de5f4c2aec84e208c34a6adecf26ce070
Mark <hoanguyen09101998@gmail.com>
2020-05-17 16:47:55 +0700

    Adding Dropdown to the Navabr

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,34 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService,
+              private accountService: AccountService) {
     
   }
-  
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+    // this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+    //   (response: IPagination) => {
+    //   this.products = response.data;
+    // }, error => {
+    //   console.log(error);
+    // });
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log("user");
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
@@ -23,11 +46,5 @@ export class AppComponent implements OnInit {
         
       })
     }
-    // this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-    //   (response: IPagination) => {
-    //   this.products = response.data;
-    // }, error => {
-    //   console.log(error);
-    // });
   }
 }
